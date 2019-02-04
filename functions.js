5599096abccca4fb32a1237d438e85f350433e94
Matthew Wehrmeyer <matthew.wehrmeyer@mrjaffesclass.com>
2019-02-04 17:03:23 +0000

    Added reduction to functions

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+let x = 1;
+
+if ( x == 0){
 let array1 = [3,6,4,2,7,8,6,10,2,5,4,1];
 for (let i=0; i<array1.length; i++) {
   array1[i] = array1[i] + 3;
@@ -17,3 +20,21 @@ console.log(newArray2);
 let array3 = ['P','a','t','r','i','c','k',' ','H','e','n','r','y'];
 let newArray3 = array3.reduce((accumulator, item) => accumulator + item, 'Go ');
 console.log(newArray3);
+}
+
+let array4 = [
+  {name: 'George', rank: 1},
+  {name: 'Thomas', rank: 2},
+  {name: 'Abraham', rank: 5},
+  {name: 'Alexander', rank: 10},
+  {name: 'Andrew', rank: 20},
+  {name: 'Ulysses', rank: 50},
+  {name: 'Benjamin', rank: 100},
+  {name: 'William', rank: 500},
+  {name: 'Grover', rank: 1000},
+  {name: 'James', rank: 5000},
+  {name: 'Salmon', rank: 10000},
+  {name: 'Woodrow', rank: 100000}
+];
+let newArray = array4.filter(item => item.rank < 50);
+console.log(newArray)
