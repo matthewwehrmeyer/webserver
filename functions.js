0850a667e6dd4cac568cd66e76d730fc6c7a8206
Matthew Wehrmeyer <matthew.wehrmeyer@mrjaffesclass.com>
2019-02-04 16:40:41 +0000

    Added new file with base code

---
+++ functions.js
@@ -0,0 +1,12 @@
+let array1 = [3,6,4,2,7,8,6,10,2,5,4,1];
+for (let i=0; i<array1.length; i++) {
+  array1[i] = array1[i] + 3;
+}
+console.log(array1);
+
+let array2 = [3,6,4,2,7,8,6,10,2,5,4,1];
+let newArray = array2.map(item => item + 3);
+console.log('Old array');
+console.log(array2);
+console.log('New array');
+console.log(newArray);
