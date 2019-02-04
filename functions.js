bf5dc53b3a3673c8a75c403b3183e53547e38123
Matthew Wehrmeyer <matthew.wehrmeyer@mrjaffesclass.com>
2019-02-04 16:46:29 +0000

    Added multiplier to functions

--- functions.js
+++ functions.js
@@ -6,7 +6,9 @@ console.log(array1);
 
 let array2 = [3,6,4,2,7,8,6,10,2,5,4,1];
 let newArray = array2.map(item => item + 3);
+let newArray2 = array2.map(item => item * 5);
 console.log('Old array');
 console.log(array2);
 console.log('New array');
 console.log(newArray);
+console.log(newArray2);
